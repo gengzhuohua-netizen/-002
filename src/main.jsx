e9f8d8887b60e3244dddb75a6504f0c20f375f09
gengzhuohua-netizen <gengzhuohua-netizen@users.noreply.github.com>
2026-06-20 17:05:03 +0800

    Remove BOM from main.jsx for Cloudflare build

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-﻿import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { createRoot } from 'react-dom/client';
 import {
   ArrowUpRight,
